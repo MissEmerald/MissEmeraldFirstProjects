ae16b68c49a6eec28fec9c0affc6fe2b8f8ef176
Stephanie Esmeralda <stephesme1710@gmail.com>
2026-08-13 19:22:21 -0600

    updated ejercicio6

--- ejercicio6_(edad_perro).py
+++ ejercicio6_(edad_perro).py
@@ -1,3 +1,8 @@
-nombre = input("Ingrese el nombre de la mascota ")
-especie = input("Ingrese la especie de la mascota ")
-edad = input("Ingrese la edad (en años) de la mascota")
+nombre = input("Ingrese el nombre de la mascota: ")
+especie = input("Ingrese la especie de la mascota: ")
+edad_perro = int(input("Ingrese la edad (en años) de la mascota: "))
+edad_humana = (edad_perro * 7)
+
+linea = "-" * 30
+
+print(f"{linea} \n FICHA DE DATOS DE LA MASCOTA \n Nombre: {nombre} \n Especie: {especie} \n Edad humana: {edad_humana}")

64f147d09e1e80a59765391456de696ad8705268
Stephanie Esmeralda <stephesme1710@gmail.com>
2026-08-24 06:26:56 -0600

    updated pract3

---
+++ Practicas/pract3.py
@@ -0,0 +1,6 @@
+while True:
+    intento = input("Ingrese contraseña: ")
+    if (intento == "Enchilada"):
+        print("Acceso concedido")
+        break
+    print("Contraseña incorrecta, inténtelo de nuevo.")

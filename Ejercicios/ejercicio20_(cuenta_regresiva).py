241e7af3c088a2caec20ac348229a24fd4ddc4c7
Stephanie Esmeralda <stephesme1710@gmail.com>
2026-08-24 06:26:12 -0600

    added ejercicio20

---
+++ Ejercicios/ejercicio20_(cuenta_regresiva).py
@@ -0,0 +1,6 @@
+conteo = int(input("Ingrese un número para la cuenta regresiva: "))
+print("Comenzando en...")
+while (conteo > 0):
+    print (f"{conteo}")
+    conteo = conteo - 1
+print("¡Despegue!")

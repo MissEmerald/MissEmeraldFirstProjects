b3068dce00a91e46c67c69ce4745c56e773ecfa9
Stephanie Esmeralda <stephesme1710@gmail.com>
2026-09-03 05:36:49 -0600

    modified ejercicio26

--- Ejercicios/ejercicio26_(adivinar_num).py
+++ Ejercicios/ejercicio26_(adivinar_num).py
@@ -1,4 +1,7 @@
 intento = int(input("Adivina el número secreto entre el 1 y el 10: "))
+
 while (intento != 7):
-    if intento < 10 
-    int(input("Número incorrecto! Intenta de nuevo: "))
+    if (intento > 10) or (intento < 1):
+        print("Ese número ni si quiera es válido, baka!")
+    intento = int(input("Número incorrecto! Intenta de nuevo: "))
+print("F-felicidades! Acertaste el número")
